afac866d131c042b620c5e194e2a51645ddfbf2c
Arnold DJAPOU <djapoujunior@gmail.com>
2022-02-15 21:52:08 +0100

    Rename doctype Traffic officer

--- warehouse_management_system/warehouse_management_system/doctype/traffic_officer_wms/test_traffic_officer_wms.py
+++ warehouse_management_system/warehouse_management_system/doctype/traffic_officer_wms/test_traffic_officer_wms.py
@@ -4,5 +4,5 @@
 # import frappe
 import unittest
 
-class TestTrafficOfficersWMS(unittest.TestCase):
+class TestTrafficOfficerWMS(unittest.TestCase):
 	pass
--- warehouse_management_system/warehouse_management_system/doctype/traffic_officer_wms/traffic_officer_wms.js
+++ warehouse_management_system/warehouse_management_system/doctype/traffic_officer_wms/traffic_officer_wms.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022, Afrikedge and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Traffic Officers WMS', {
+frappe.ui.form.on('Traffic Officer WMS', {
 	// refresh: function(frm) {
 
 	// }

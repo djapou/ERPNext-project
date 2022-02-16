a62f9094196cde3397efd9a2d4e97d17a3c3d0ed
Arnold DJAPOU <djapoujunior@gmail.com>
2022-02-16 17:40:17 +0100

    Add Bill Inbound doctype

---
+++ warehouse_management_system/warehouse_management_system/doctype/bill_inbound_wms/bill_inbound_wms.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Afrikedge and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bill Inbound WMS', {
+	// refresh: function(frm) {
+
+	// }
+});

fd1641a08bc00389d2149c86ac9dcb6f6a7cea31
Arnold DJAPOU <djapoujunior@gmail.com>
2022-02-17 17:37:30 +0100

    add doctypes Charge for logistic service AND Physical taily sheet

---
+++ warehouse_management_system/warehouse_management_system/doctype/charge_for_logistic_service_wms/charge_for_logistic_service_wms.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Afrikedge and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Charge For Logistic Service WMS', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ warehouse_management_system/warehouse_management_system/doctype/physical_taily_sheet_wms/physical_taily_sheet_wms.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Afrikedge and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('PhysIcal Taily Sheet WMS', {
+	// refresh: function(frm) {
+
+	// }
+});

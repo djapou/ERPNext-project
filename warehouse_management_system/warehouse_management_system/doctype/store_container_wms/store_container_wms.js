f7ab98433f480bcc0d720750283c427bc767679e
Arnold DJAPOU <djapoujunior@gmail.com>
2022-02-17 14:18:28 +0100

    add doctype store container

---
+++ warehouse_management_system/warehouse_management_system/doctype/store_container_wms/store_container_wms.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Afrikedge and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Store Container WMS', {
+	// refresh: function(frm) {
+
+	// }
+});

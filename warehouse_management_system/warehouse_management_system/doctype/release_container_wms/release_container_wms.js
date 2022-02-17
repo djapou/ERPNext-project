ba01a07eedc5cc7b0349a045f3cf8744e2cfee77
Arnold DJAPOU <djapoujunior@gmail.com>
2022-02-17 21:34:26 +0100

    Add doctype release container

---
+++ warehouse_management_system/warehouse_management_system/doctype/release_container_wms/release_container_wms.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Afrikedge and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Release Container WMS', {
+	// refresh: function(frm) {
+
+	// }
+});

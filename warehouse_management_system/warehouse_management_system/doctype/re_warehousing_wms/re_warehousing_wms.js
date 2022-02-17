dc0617e3734ba63a8ec1a650dcd343639d06cecf
Arnold DJAPOU <djapoujunior@gmail.com>
2022-02-17 13:35:51 +0100

    Add doctypes Revise bill AND Re_Warehousing

---
+++ warehouse_management_system/warehouse_management_system/doctype/re_warehousing_wms/re_warehousing_wms.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Afrikedge and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Re Warehousing WMS', {
+	// refresh: function(frm) {
+
+	// }
+});
